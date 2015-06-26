9c954542ad2c126ca63abb43e621a297c6053f94
Adrian Ossi <adrian.ossi@gmail.com>
2015-06-25 19:48:43 -0500

    initial commit

---
+++ src/main/java/org/letsdig/app/LetsDigApplication.java
@@ -0,0 +1,12 @@
+package org.letsdig.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsDigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsDigApplication.class, args);
+    }
+}
