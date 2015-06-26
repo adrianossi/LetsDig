c713d51d37345203972943a5df0a995fa8bb464f
Adrian Ossi <adrian.ossi@gmail.com>
2015-06-26 12:13:06 -0500

    Add repository and transactional annotations to ProjectDao class

--- src/main/java/org/letsdig/app/models/dao/ProjectDao.java
+++ src/main/java/org/letsdig/app/models/dao/ProjectDao.java
@@ -3,10 +3,16 @@ package org.letsdig.app.models.dao;
 import org.letsdig.app.models.LatLong;
 import org.letsdig.app.models.Project;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 /**
  * Created by adrian on 6/5/15.
  */
+
+@Transactional
+@Repository
 public interface ProjectDao extends CrudRepository<Project, Integer> {
 
     Project findByName(String name);
